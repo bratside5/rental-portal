abf954b9e4a660f15b85afa990c4f806fa58518a
bratside5 <bratside5@gmail.com>
2020-04-30 15:53:42 +0200

    Add Firebase Authentication

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import {
+  FirebaseAuthConsumer,
+  IfFirebaseAuthed,
+  IfFirebaseAuthedAnd,
+} from "@react-firebase/auth";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Changed this text to test pull request dev deploy</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <button
+        onClick={() => {
+          const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+          firebase.auth().signInWithPopup(googleAuthProvider);
+        }}
+      >
+        Sign In with Google
+      </button>
+      <button
+        data-testid="signin-anon"
+        onClick={() => {
+          firebase.auth().signInAnonymously();
+        }}
+      >
+        Sign In Anonymously
+      </button>
+      <button
+        onClick={() => {
+          firebase.auth().signOut();
+        }}
+      >
+        Sign Out
+      </button>
+      <FirebaseAuthConsumer>
+        {({ isSignedIn, user, providerId }) => {
+          return (
+            <pre style={{ height: 300, overflow: "auto" }}>
+              {JSON.stringify({ isSignedIn, user, providerId }, null, 2)}
+            </pre>
+          );
+        }}
+      </FirebaseAuthConsumer>
+      <div>
+        <IfFirebaseAuthed>
+          {() => {
+            return <div>You are authenticated</div>;
+          }}
+        </IfFirebaseAuthed>
+        <IfFirebaseAuthedAnd
+          filter={({ providerId }) => providerId !== "anonymous"}
         >
-          Learn React
-        </a>
-      </header>
+          {({ providerId }) => {
+            return <div>You are authenticated with {providerId}</div>;
+          }}
+        </IfFirebaseAuthedAnd>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyCFELCTHbwwIjxr70jjI5wjkh9gapwH1yE",
+  authDomain: "property-portal-development.firebaseapp.com",
+  databaseURL: "https://property-portal-development.firebaseio.com",
+  projectId: "property-portal-development",
+  storageBucket: "property-portal-development.appspot.com",
+  messagingSenderId: "256506708080",
+  appId: "1:256506708080:web:01b781142ccf9c3eafb4b5",
+  measurementId: "G-QF4162J2BV",
+};
+
+export default config;
